de7560215d14b1b79d7f5868f2557c2ed1f8d8c1
Logan Bell <loganbell@gmail.com>
2017-08-24 07:18:41 -0700

    Adding a qsort wrapper and calculating length on the fly

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
     let mut numbers = vec![30, 25, 10, 40, 99, 80, 33];
-    qsort_it(&mut numbers, 0, 6);
-
+    qsort(&mut numbers);
     println!("Sorted values are {:?}", numbers);
+}
 
+fn qsort(a: &mut Vec<i32>) {
+    let length = a.len() - 1;
+    qsort_it(a, 0, length);
 }
 
 fn qsort_it(a: &mut Vec<i32>, left: usize, right: usize) {
@@ -46,7 +49,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut numbers = vec![1, 5, 3, 4, 10, 20, 15];
-        qsort_it(&mut numbers, 0, 6);
+        qsort(&mut numbers);
         assert_eq!(numbers, vec![1, 3, 4, 5, 10, 15, 20]);
     }
 }
